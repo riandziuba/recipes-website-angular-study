140eef7a3aad16a7e56b4c8eb640e39fd028eea1
riandziuba <riandziuba@gmail.com>
2019-11-21 21:20:41 -0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trab-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'trabalho';
+}
